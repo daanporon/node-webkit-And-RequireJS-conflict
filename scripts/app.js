6399146edae961764e681bb943eebe8e6ad29182
Daan Poron <daan@poron.be>
2012-12-13 13:02:57 +0100

    add files

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+require([], function() {
+
+    var gui = window.requireNode('nw.gui');
+
+    gui.Shell.openExternal('http://www.google.be');
+
+});
---
+++ scripts/config.js
@@ -0,0 +1,15 @@
+require.config({
+
+    baseUrl: './scripts',
+
+    deps: ["app"],
+
+    // 3rd party script alias names (Easier to type "jquery" than "libs/jquery-1.7.2.min")
+    paths: {
+    },
+
+    // Sets the configuration for your third party scripts that are not AMD compatible
+    shim: {
+    }
+
+});
